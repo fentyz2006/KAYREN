8647d5e5619250d772264ea7e134703a8f3f7a2b
fentyz2006 <136098897+fentyz2006@users.noreply.github.com>
2024-12-16 06:17:59 -0500

    change

--- src/components/CalendarView.js
+++ src/components/CalendarView.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const localizer = momentLocalizer(moment);
 
-const CalendarView = ({ events }) => {
-  return (
-    <Calendar
-      localizer={localizer}
-      events={events}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 500 }}
-    />
-  );
-};
+const CalendarView = ({ events }) => (
+  <Calendar
+    localizer={localizer}
+    events={events}
+    startAccessor="start"
+    endAccessor="end"
+    style={{ height: 500 }}
+  />
+);
 
 export default CalendarView;
